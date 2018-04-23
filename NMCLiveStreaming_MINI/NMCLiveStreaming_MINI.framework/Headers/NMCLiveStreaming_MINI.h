7a2c608247a7a02c5e17f23a2960dfa59e592994
taojinliang <251094607@qq.com>
2018-04-23 15:41:52 +0800

    更新版本

---
+++ NMCLiveStreaming_MINI/NMCLiveStreaming_MINI.framework/Headers/NMCLiveStreaming_MINI.h
@@ -0,0 +1,31 @@
+//
+//  NMCLiveStreaming_MINI.h
+//  NMCLiveStreaming_MINI
+//
+//  Created by taojinliang on 2018/4/23.
+//  Copyright © 2018年 taojinliang. All rights reserved.
+//
+
+#ifndef NMCLiveStreaming_MINI_h
+#define NMCLiveStreaming_MINI_h
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NMCLiveStreaming.
+FOUNDATION_EXPORT double NMCLiveStreamingVersionNumber;
+
+//! Project version string for NMCLiveStreaming.
+FOUNDATION_EXPORT const unsigned char NMCLiveStreamingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NMCLiveStreaming/PublicHeader.h>
+
+
+#import <NMCLiveStreaming_MINI/LSMediaCapture.h>
+#import <NMCLiveStreaming_MINI/LSAudioParaCtxConfiguration.h>
+#import <NMCLiveStreaming_MINI/LSVideoParaCtxConfiguration.h>
+#import <NMCLiveStreaming_MINI/LSLiveStreamingParaCtxConfiguration.h>
+#import <NMCLiveStreaming_MINI/nMediaLiveStreamingDefs.h>
+#import <NMCLiveStreaming_MINI/LSStatisticsObject.h>
+
+
+#endif /* NMCLiveStreaming_MINI_h */
